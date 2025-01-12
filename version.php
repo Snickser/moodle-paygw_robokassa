13e2a73a6110f25326a6f09eb9f3f1686a10b672
Snickser <snickser@gmail.com>
2025-01-12 08:18:54 +0300

    version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025010100;
+$plugin->version   = 2025011200;
 $plugin->requires  = 2023100900;
 $plugin->component = 'paygw_robokassa';
 $plugin->release   = '2.8';
