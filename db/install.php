5a3773afa9aca8788d6f61e44d3e577c7277ee31
Snickser <snickser@gmail.com>
2025-03-06 11:23:25 +0300

    fix

--- db/install.php
+++ db/install.php
@@ -22,10 +22,16 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+/**
+ * Enabler
+ *
+ * @return boolean
+ */
 function xmldb_paygw_robokassa_install() {
     global $CFG;
 
     // Enable the bank payment gateway on installation. It still needs to be configured and enabled for accounts.
     $order = (!empty($CFG->paygw_plugins_sortorder)) ? explode(',', $CFG->paygw_plugins_sortorder) : [];
     set_config('paygw_plugins_sortorder', join(',', array_merge($order, ['robokassa'])));
+    return true;
 }
